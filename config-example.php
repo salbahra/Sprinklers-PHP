5ce75c01d29c81a4fdd2c1f3b1c18b28fe0c6561
Samer Albahra <salbahra@gmail.com>
2013-06-15 13:57:38 -0300

    Example configuration file

---
+++ config-example.php
@@ -0,0 +1,30 @@
+<?php
+
+#Set timezone
+date_default_timezone_set('US/Central');
+
+#WebApp Title
+$webtitle = "Sprinkler System";
+
+#Set IP of OpenSprinkler
+$os_ip = "192.168.1.30";
+
+#Set OpenSprinkler password
+$os_pw = "opendoor";
+
+#How far back to show logs ex. '24 hours' or '14 days'
+$timeViewWindow="7 days";
+
+#Password File
+$pass_file="/var/www/sprinklers/.htpasswd";
+
+#Cache File
+$cache_file = "/var/www/sprinklers/.cache";
+
+#Sprinkler Log File
+$log_file = "/var/www/sprinklers/SprinklerChanges.txt";
+
+#Sprinkler Previous Status
+$log_previous = "/var/www/sprinklers/SprinklerPrevious.txt";
+
+?>
