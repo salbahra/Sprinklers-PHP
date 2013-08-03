a2e7728598a1995158d32b795c74c37bf1ac801f
Samer Albahra <salbahra@gmail.com>
2013-08-03 06:56:35 -0500

    Stations: Fix for users using PHP < 5.3.0

--- main.php
+++ main.php
@@ -1580,4 +1580,33 @@ function sec2hms($diff) {
     return $str.($minutes < 10 ? "0".$minutes : $minutes).":".($seconds < 10 ? "0".$seconds : $seconds);
 }
 
+#Fix for PHP <5.3.0
+function str_getcsv4($input, $delimiter = ',', $enclosure = '"') {
+
+    if( ! preg_match("/[$enclosure]/", $input) ) {
+      return (array)preg_replace(array("/^\\s*/", "/\\s*$/"), '', explode($delimiter, $input));
+    }
+
+    $token = "##"; $token2 = "::";
+    //alternate tokens "\034\034", "\035\035", "%%";
+    $t1 = preg_replace(array("/\\\[$enclosure]/", "/$enclosure{2}/",
+         "/[$enclosure]\\s*[$delimiter]\\s*[$enclosure]\\s*/", "/\\s*[$enclosure]\\s*/"),
+         array($token2, $token2, $token, $token), trim(trim(trim($input), $enclosure)));
+
+    $a = explode($token, $t1);
+    foreach($a as $k=>$v) {
+        if ( preg_match("/^{$delimiter}/", $v) || preg_match("/{$delimiter}$/", $v) ) {
+            $a[$k] = trim($v, $delimiter); $a[$k] = preg_replace("/$delimiter/", "$token", $a[$k]); }
+    }
+    $a = explode($token, implode($token, $a));
+    return (array)preg_replace(array("/^\\s/", "/\\s$/", "/$token2/"), array('', '', $enclosure), $a);
+
+}
+
+if ( ! function_exists('str_getcsv')) {
+  function str_getcsv($input, $delimiter = ',', $enclosure = '"') {
+    return str_getcsv4($input, $delimiter, $enclosure);
+  }
+}
+
 ?>
